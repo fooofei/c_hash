e88a424850409efec5418ceff2fcc028a40c656a
fooofei <aihujianfei@gmail.com>
2019-12-21 20:38:53 +0800

    sync

--- xhash/xhash.c
+++ xhash/xhash.c
@@ -9,9 +9,9 @@
  * /
 
 /** 
- * |---------------|-----|----------|----------|---------------|
+ * |---------------|----------|----------|----------|---------------|
  *                        
- *   0-3 read count 4 is tail 5 write
+ *   0-3read count 4dowrite    5is_tail 
  *  最多支持 2^4 -1=15 个线程同时读取
  */
 union xhash_key {
@@ -23,7 +23,9 @@ union xhash_key {
         uint64_t data : 58;
     };
 };
+
 #define DATA_MASK 0x3FFFFFFFFFFFFFF
+
 static inline uint64_t make_key(uint64_t rd_cnt, uint64_t write, uint64_t tail, uint64_t data)
 {
     uint64_t v1 = (0xF & rd_cnt) << 60;
@@ -33,54 +35,66 @@ static inline uint64_t make_key(uint64_t rd_cnt, uint64_t write, uint64_t tail, 
     uint64_t h = (v1 | v2 | v3 | v4);
     return h;
 }
+
 static inline uint64_t key_get_rd_cnt(uint64_t key)
 {
     return (key >> 60) & 0xF;
 }
+
 static inline uint64_t key_get_write(uint64_t key)
 {
     return (key >> 59) & 0x1;
 }
+
 static inline uint64_t key_get_tail(uint64_t key)
 {
     return (key >> 58) & 0x1;
 }
+
 static inline uint64_t key_get_data(uint64_t key)
 {
     return (key & DATA_MASK);
 }
+
 static inline void key_clear_rd_cnt(uint64_t* key)
 {
     *key = ((*key) & 0xFFFFFFFFFFFFFFF);
 }
+
 static inline void key_clear_write(uint64_t* key)
 {
     // only write bit=0
-    *key = (((*key) & 0xF7FFFFFFFFFFFFFF);
+    *key = ((*key) & 0xF7FFFFFFFFFFFFFF);
 }
+
 static inline void key_clear_tail(uint64_t* key)
 {
     *key = ((*key) & 0xFBFFFFFFFFFFFFFF);
 }
+
 static inline void key_clear_data(uint64_t* key)
 {
     *key = ((*key) & 0xFC00000000000000);
 }
+
 static inline void key_set_rd_cnt(uint64_t* key, uint64_t rd_cnt)
 {
     key_clear_rd_cnt(key);
     *key |= ((0xF & rd_cnt) << 60);
 }
+
 static inline void key_set_write(uint64_t* key, uint64_t write)
 {
     key_clear_write(key);
     *key |= ((0x1 & write) << 59);
 }
+
 static inline void key_set_tail(uint64_t* key, uint64_t tail)
 {
     key_clear_tail(key);
     *key |= ((0x1 & tail) << 58);
 }
+
 static inline void key_set_data(uint64_t* key, uint64_t data)
 {
     key_clear_data(key);
@@ -91,43 +105,73 @@ struct xhash_entry {
     uint64_t key;
     void* data;
 };
+
 struct xhash {
-    uint32_t capacity;
-    uint32_t used;
+    uint64_t capacity;
+    uint64_t used;
     struct xhash_entry entry[0];
 };
+
+bool atomic_cmp_set(uint64_t* dst, uint64_t src, uint64_t new_)
+{
+    // TODO implement this.
+    *dst = new_;
+    return true;
+}
+
+uint64_t atomic_get(uint64_t* dst)
+{
+    // TODO implement this.
+    return *dst;
+}
+
+void atomic_add(uint64_t * dst)
+{
+    // TODO implement this.
+    *dst += 1;
+}
 
 int xhash_add(struct xhash* hash, uint64_t key, void* data)
 {
     uint32_t idx = 0;
     struct xhash_entry* en = 0;
-    const double max_load_factor = 0.7;
 
-    uint64_t h = ((union xhash_key*)&key)->data;
-    idx = h % hash->capacity;
-
-    for (; 0 != get_do_write(hash->entry[idx].key);) {
+    uint64_t used = atomic_get(&hash->used);
+    uint64_t cap = atomic_get(&hash->capacity);
+    cap = (uint64_t)((double)cap * 0.7);
+    if (used >= cap) {
+        // not have enough slots. 
+        return -1;
     }
-    //
-    // set do write=1
 
-    // wait read cnt=0
+    uint64_t key_data = key_get_data(key);
+    idx = key_data % hash->capacity;
 
-    _sync_test_and_set()
-
-        if (h->used >= (h->capacity * max_load_factor))
+    // wait do write = 0
+    do
     {
-        /** no have enough slots. */
-        return -1;
-    }
-    if (key == 0) {
-        return -1;
-    }
+        uint64_t tmp = atomic_get(&(hash->entry[idx].key));
+        uint64_t expect = tmp;
+        key_clear_write(&expect);
+        uint64_t new_ = tmp;
+        key_set_write(&new_, 1);
+    } while (atomic_cmp_set(&(hash->entry[idx].key), expect, new_));
+    
+    // wait read cnt = 0
+    do
+    {
+        uint64_t tmp = atomic_get(&(hash->entry[idx].key));
+        uint64_t expect = tmp;
+        key_clear_rd_cnt(&expect);
+        uint64_t new_ = expect;
+    } while (atomic_cmp_set(&(hash->entry[idx].key), expect, new_));
+
     index = key % h->capacity;
     en = &h->entry[index];
     en->key = key;
     en->data = data;
-    __sync_add_and_fetch(&h->used, 1);
+    
+    atomic_add(&hash->used);
     return 0;
 }
 
@@ -136,7 +180,7 @@ static inline uint64_t xhash_key_collision(struct xhash* h, uint64_t key, uint32
     return ((key % h->size) + i) % h->size;
 }
 
-struct xhash* xhash_ctor(uint32_t size)
+struct xhash* xhash_ctor(int size)
 {
     struct xhash* h = 0;
     h = calloc(1, sizeof(struct xhash) + size * sizeof(struct xhash_entry));
@@ -147,6 +191,11 @@ struct xhash* xhash_ctor(uint32_t size)
 void xhash_dtor(struct xhash* h)
 {
     free(h);
+}
+
+void* xhash_search(struct xhash* h, uint64_t key)
+{
+
 }
 
 bool xhash_exists(struct xhash* h, uint64_t key)
--- xhash/xhash.h
+++ xhash/xhash.h
@@ -6,7 +6,8 @@
 
 struct xhash;
 
-struct xhash* xhash_ctor(uint32_t size);
+struct xhash* xhash_ctor(int size);
+// key 会丢失一部分位
 int xhash_add(struct xhash* h, uint64_t key, void* data);
 void* xhash_search(struct xhash* h, uint64_t key);
 void xhash_delete(struct xhash* h, uint64_t key);
